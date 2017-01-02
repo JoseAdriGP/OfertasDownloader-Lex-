2060f86e87ba9d406ad3903ebffb96c3eed0108a
José Adrián Garrido Puertas <adryx@correo.ugr.es>
2017-01-02 17:01:33 +0100

    Add files via upload

---
+++ downloader.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ wget https://www.instant-gaming.com/es/juegos/steam/ -O steam.txt
+ wget https://www.instant-gaming.com/es/juegos/origin/ -O origin.txt
+ wget https://www.instant-gaming.com/es/juegos/battle-net/ -O battle-net.txt
